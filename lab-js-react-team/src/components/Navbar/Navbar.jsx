2aee38b74596ac2d8354673c8e5ed3b201d279bb
ecemonkol <ecemonkol@gmail.com>
2024-04-17 21:28:26 +0200

    add navbar, header, footer

---
+++ lab-js-react-team/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <header className="Navbar">
+      <h1>Navbar</h1>
+    </header>
+  );
+}
+
+export default Navbar;
